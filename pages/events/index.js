e1fdf9189ccc76cff678212d5f1085a1d86be3fc
John Xander <johnblox5@gmail.com>
2022-12-28 18:40:18 +0100

    add info to events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,6 +1,22 @@
 const Page = () => {
     return (
-        <h1>Evens</h1>
+        <div>
+            <h1>Events Page</h1>
+            <div>
+                <a href=''>
+                    <img src="" alt="" />
+                    <h2>Events in London</h2>
+                </a>
+                <a href=''>
+                    <img src="" alt="" />
+                    <h2>Events in San Francisco</h2>
+                </a>
+                <a href=''>
+                    <img src="" alt="" />
+                    <h2>Events in Barcelona</h2>
+                </a>
+            </div>
+        </div>
     )
 }
 
