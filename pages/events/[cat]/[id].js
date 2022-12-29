fa681fd014c435d22ded2841ae79ef90bd41b08d
John Xander <johnblox5@gmail.com>
2022-12-29 18:21:59 +0100

    render individual events

--- pages/events/[cat]/[id].js
+++ pages/events/[cat]/[id].js
@@ -1,7 +1,46 @@
-const EventPage = () => {
+import Image from 'next/image'
+
+const EventPage = ({ data }) => {
+    console.log(data)
     return (
-        <h1>Our single event</h1>
+        <div>
+            <Image
+                src={data.image}
+                width={1000}
+                height={500}
+                alt={data.title}
+            />
+            <h1>{data.title}</h1>
+            <p>{data.description}</p>
+        </div>
     )
 }
 
 export default EventPage
+
+export async function getStaticPaths() {
+    const { allEvents } = await import('/data/data.json')
+    const allPaths = allEvents.map(path => {
+        return {
+            params: {
+                cat: path.city,
+                id: path.id
+            }
+        }
+    })
+
+    return {
+        paths: allPaths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps(context) {
+    const id = context.params.id
+    const { allEvents } = await import('/data/data.json')
+    const eventData = allEvents.find(ev => id === ev.id)
+    
+    return {
+        props: { data: eventData }
+    }
+}
