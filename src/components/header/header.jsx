5818d607a63f719c6c957fca9d6e223c7a533282
John Xander <johnblox5@gmail.com>
2022-12-29 19:44:22 +0100

    add logo to header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,14 +1,24 @@
+import Image from "next/image"
 import Link from "next/link"
 
 export const Header = () => {
     return (
         <header>
-            <nav>
-                <img src="" alt="" />
-                <Link href="/">Home</Link>
-                <Link href="/events">Events</Link>
-                <Link href="/about-us">About Us</Link>
-            </nav>
+            <div>
+                <Image
+                    src={'/images/logo.png'}
+                    width={50}
+                    height={50}
+                    alt={'logo'}
+                />
+                <nav>
+                    <img src="" alt="" />
+                    <Link href="/">Home</Link>
+                    <Link href="/events">Events</Link>
+                    <Link href="/about-us">About Us</Link>
+                </nav>
+            </div>
+            <h1>Bla Bla Bla</h1>
       </header>
     )
 }
