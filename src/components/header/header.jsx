5a15ffa273d96c27b615a189ad4f36bbc3d9254f
John Xander <johnblox5@gmail.com>
2022-12-30 15:08:15 +0100

    update header margin

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,28 +4,30 @@ import Link from "next/link"
 export const Header = () => {
     return (
         <header>
-            <div className="topNav">
-                <Image
-                    src={'/images/logo.png'}
-                    width={40}
-                    height={40}
-                    alt={'logo'}
-                />
-                <nav>
-                    <ul>
-                        <li>
-                            <Link href="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link href="/events">Events</Link>
-                        </li>
-                        <li>
-                            <Link href="/about-us">About Us</Link>
-                        </li>
-                    </ul>
-                </nav>
+            <div>
+                <div className="topNav">
+                    <Image
+                        src={'/images/logo.png'}
+                        width={40}
+                        height={40}
+                        alt={'logo'}
+                    />
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link href="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link href="/events">Events</Link>
+                            </li>
+                            <li>
+                                <Link href="/about-us">About Us</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <h1>John Xander's Events App</h1>
             </div>
-            <h1>John Xander's Events App</h1>
       </header>
     )
 }
